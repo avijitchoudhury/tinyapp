b75b0f7ba2a223788be3350672530e2c103cc04c
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-30 20:25:24 +0000

    modified to add header URL format

--- express_server.js
+++ express_server.js
@@ -30,6 +30,12 @@ app.get("/urls", (req, res) => { //passing the URL data to our template
   res.render('urls_index', templateVars)
 })
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+    res.render("urls_show", templateVars)
+})
+
 // app.get("/set", (req, res) => {
 //   const a = 1;
 //   res.send(`a = ${a}`);
