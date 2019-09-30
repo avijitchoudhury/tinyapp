506df63692b27a3c0e84f6e21b2fd18fbaedd040
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-30 18:42:12 +0000

    tested two routes of set & fetch

--- express_server.js
+++ express_server.js
@@ -11,7 +11,7 @@ app.get("/", (req, res) => { //prints the initial message Hello when opening the
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { //when turning on the server to recieve requests. the console will print out message
   console.log(`Example app listening on port ${PORT}!`);
 });
 
@@ -21,4 +21,13 @@ app.get("/urls.json", (req, res) => { //prints the urlDatabase in the browser
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n"); //should see Hello World w/ world in BOLD
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
 });
